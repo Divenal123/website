b84e09531327ba4e3bdd3320ab5f87e78fc41782
igorvisi <mail@igorvisi.com>
2019-11-18 14:55:15 +0100

    Ajout du tech challenge

--- src/pages/Challenge.vue
+++ src/pages/Challenge.vue
@@ -3,9 +3,23 @@
     <section class="section-margin">
       <div class="container">
         <div class="section-intro text-center pb-98px">
-          <p class="section-intro__title">Challengez-vous et tentez de rapporter la cagnote</p>
-          <h2 class="primary-text">Ouverture pour bientôt</h2>
+          <h2 class="primary-text">Tech Challenge</h2>
           <g-image src="~/assets/img/home/section-style.png" alt />
+          <p
+            class="section-intro__title"
+          >Soumettez votre projet tech et tentez de remporter la cagnote</p>
+          <g-image src="~/assets/img/logo/appchallenge.png" class="img-fluid mt-4" alt />
+          <g-image src="~/assets/img/gallery/price.jpg" class="img-fluid mt-3" alt />
+          <p>Les vainqueurs du Tech Challenge édition JI 12</p>
+          <div class="row">
+            <div class="col-12 text-center">
+              <a
+                class="button mb-2"
+                target="_blank"
+                href="https://app.journeesinformatiques.com"
+              >Je veux inscrire mon application</a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
